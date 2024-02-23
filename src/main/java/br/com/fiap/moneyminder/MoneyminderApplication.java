8bd3864c6a80446764c916906425aa2ef14a8387
joaocarloslima <joaocarloslima@me.com>
2024-02-23 09:58:30 -0300

    Criação do projeto spring

---
+++ src/main/java/br/com/fiap/moneyminder/MoneyminderApplication.java
@@ -0,0 +1,22 @@
+package br.com.fiap.moneyminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class MoneyminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyminderApplication.class, args);
+	}
+
+	@RequestMapping("/home")
+	public String home(){
+		return "ok";
+	}
+
+
+}
---
+++ src/test/java/br/com/fiap/moneyminder/MoneyminderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.moneyminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
