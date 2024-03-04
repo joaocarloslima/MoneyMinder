1f3396fd918023c756ce0907276b0410d441ae4e
joaocarloslima <joaocarloslima@me.com>
2024-03-04 09:10:16 -0300

    criação dos mapeamentos delete e put

--- src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
@@ -2,14 +2,17 @@ package br.com.fiap.moneyminder.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,23 +46,54 @@ public class CategoriaController {
     public ResponseEntity<Categoria> get(@PathVariable Long id) {
         log.info("Buscar por id: {}", id);
 
-        // for (Categoria categoria : repository ){
-        // if (categoria.id().equals(id)){
-        // return ResponseEntity
-        // .status(HttpStatus.OK)
-        // .body(categoria);
-        // }
-        // }
-
-        var optionalCategoria = repository
-                .stream()
-                .filter(c -> c.id().equals(id))
-                .findFirst();
+        var optionalCategoria = buscarCategoriaPorId(id);
 
         if (optionalCategoria.isEmpty())
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(optionalCategoria.get());
+    }
+
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        log.info("apagando categoria {}", id);
+
+        var optionalCategoria = buscarCategoriaPorId(id);
+
+        if (optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        repository.remove(optionalCategoria.get());
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Categoria categoria){
+        log.info("atualizando categoria id {} para {}", id, categoria);
+        
+        var optionalCategoria = buscarCategoriaPorId(id);
+
+        if (optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        var categoriaEncontrada = optionalCategoria.get();
+        var categoriaAtualizada = new Categoria(id, categoria.nome(), categoria.icone());
+        repository.remove(categoriaEncontrada);
+        repository.add(categoriaAtualizada);
+
+        return ResponseEntity.ok().body(categoriaAtualizada);
+    }
+
+    private Optional<Categoria> buscarCategoriaPorId(Long id) {
+        var optionalCategoria = repository
+                .stream()
+                .filter(c -> c.id().equals(id))
+                .findFirst();
+        return optionalCategoria;
     }
 
 }
