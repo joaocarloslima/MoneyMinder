ad53639136cc60b1c0303162e32ff34b9ec717d3
joaocarloslima <joaocarloslima@me.com>
2024-04-12 09:05:53 -0300

    validação de categoria

--- src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
@@ -21,6 +21,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.moneyminder.model.Categoria;
 import br.com.fiap.moneyminder.repository.CategoriaRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -38,7 +39,7 @@ public class CategoriaController {
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public Categoria create(@RequestBody Categoria categoria) {
+    public Categoria create(@RequestBody @Valid Categoria categoria) {
         log.info("cadastrando categoria: {}", categoria);
         return categoriaRepository.save(categoria);
     }
